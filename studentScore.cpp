aab3035894e05d38a964bfeda5754ee405ac75dd
sunpool <chengbohan@gmail.com>
2020-08-13 15:59:04 -0400

    Exception, grade fns, basic output

--- studentScore.cpp
+++ studentScore.cpp
@@ -15,6 +15,13 @@ using std::setprecision;
 using std::string;
 using std::vector;
 
+struct Student_info
+{
+    string name;
+    double midterm, final;
+    vector<double> hws;
+};
+
 double median(vector<double> vec)
 {
     typedef vector<double>::size_type vec_sz;
@@ -22,6 +29,9 @@ double median(vector<double> vec)
     if ( size == 0)
         throw std::domain_error("Median of empty vector is undefinded");
 
+    // auto cmp = [](const Student_info& a, const Student_info& b) {
+    //     return a.name < b.name;
+    // }
     std::sort(begin(vec), end(vec));
     vec_sz mid = size / 2;
 
@@ -41,30 +51,61 @@ double grade(double mid, double final, const vector<double>& hws)
     return grade(mid, final, median(hws));
 }
 
+double grade( const Student_info& s)
+{
+    return grade(s.midterm, s.final, s.hws);
+}
+
+std::istream& read_hw(std::istream& in, vector<double>& hws)
+{
+    if (in)
+    {
+        hws.clear();
+        double hw;
+        while (in >> hw)
+            hws.push_back(hw);
+        in.clear();
+    }
+    return in;
+}
+
+std::istream& read(std::istream& in, Student_info& s)
+{
+    cout << "Enter Name, midterm score, and final score: " << endl;
+    in >> s.name >> s.midterm >> s.final;
+    cout << "Enter homework scores: " << endl;
+    read_hw(in, s.hws);
+    return in;
+}
+
 int main()
 {
-    cout << "enter student name: " << endl;
-    string name;
-    cin >> name;
+    vector<Student_info> students;
+    Student_info sif;
 
-    double midterm, final;
-    cout << "enter midterm and final exam score: " << endl;
-    cin >> midterm >> final;
-    cout << midterm << final << endl;
-
-    vector<double> hws;
-    double hw;
-    cout << "Enter all homework grades, " "end by end of file: ";
-    while (cin >> hw)
+    while ( read(cin, sif))
     {
-        hws.push_back(hw);
+        students.push_back(sif);
     }
-    std::copy(hws.begin(), hws.end(), std::ostream_iterator<double>(cout, ", "));
 
     streamsize prec = cout.precision();
-    cout << "Your final grade is " << setprecision(3)
-         << grade(midterm, final, hws)
-         << setprecision(prec) << endl;
+    cout << setprecision(3);
+    // cout << "size of students: " << students.size() << endl;
 
+    for (auto& s : students)
+    {
+        try
+        {
+            double final_grade = grade(s);
+            cout << s.name << ": " << final_grade << endl;
+        }
+        catch (std::domain_error e)
+        {
+            cout << "Error: " << endl;
+            cout << e.what() << endl;
+        }
+    }
+    cout << setprecision(prec) << endl;
+    cout << "完成!" << endl;
     return 0;
 }
